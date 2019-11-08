2ffa01ff5ba1982311cffb1933a49e8450e9b98b
oggyst <51537934+oggyst@users.noreply.github.com>
2019-11-08 14:37:30 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,12 +1,24 @@
 let arrCategories = [];
 let boolLogedIn = false;
+let intUserID;
 let strChossenUser;
-let arrBoughtProducts = [[]];
+let dataProducts;
+let strCategories;
+let strBoughtProducts = [];
 function buy (x)
 {
 	if (boolLogedIn)
 	{
-		arrBoughtProducts[strChossenUser][x] = document.getElementById("st-quantity" + x).value
+		strBoughtProducts[strBoughtProducts.length] = intUserID;
+		for (var j = 0; j < dataProducts.value.length; j++)
+		{
+			if (dataProducts.value[j].ProductID == x)
+			{
+			strBoughtProducts[strBoughtProducts.length] = dataProducts.value[j].ProductName;
+			strBoughtProducts[strBoughtProducts.length] = dataProducts.value[j].UnitPrice;
+			}
+		}
+		strBoughtProducts[strBoughtProducts.length] = document.getElementById("st-quantity" + (x)).value;
 	}
 	else
 	{
@@ -16,10 +28,34 @@ function buy (x)
 }
 function shopingCart () 
 {
-
+	let intTotalProduct, intTotalCart = 0;
+	let strShopingCart = '<h1> Currently in your shoping cart is: </h1>';
+	for(var i = 0; i < strBoughtProducts.length;i+=4)
+	{ 
+		intTotalProduct = 0;
+		if (intUserID == strBoughtProducts[i])
+		{
+		intTotalProduct = strBoughtProducts[i+2] * strBoughtProducts[i+3];
+		strShopingCart += '<div class = "st-product">' + strBoughtProducts[i+1] + '</br>' + Math.floor(strBoughtProducts[i+3]) + ' units, total: ' + Math.floor(intTotalProduct)+ ' </div>';
+		}
+		intTotalCart += intTotalProduct;
+	}
+	if (strShopingCart != '<h1> Currently in your shoping cart is: </h1>')
+	{
+		document.getElementById("st-select").innerHTML = "";
+		strShopingCart += '<div class = "st-back" onclick="goBack()"> Back </div><div class = "st-total">Total cost is: ' + Math.floor(intTotalCart) + '€';
+		document.getElementById("st-content").innerHTML = (strShopingCart);
+	}
+}
+function goBack ()
+{
+	document.getElementById("st-content").innerHTML = '<div id ="st-select"></div><div id = "st-content></div>';
+	 document.getElementById("st-select").innerHTML = strCategories;
+	 document.getElementById("st-content").innerHTML = arrCategories[arrCategories.length-1];
 }
 function onChange(x) {
-	document.getElementById("st-content").innerHTML = (arrCategories[x.value]);
+	
+		document.getElementById("st-content").innerHTML = (arrCategories[x.value]);
 }
 function closeForm ()
 {
@@ -40,10 +76,44 @@ function login ()
 		'<input type="button" value="log out" onclick="logOut()">')	
 	}
 }
+function logOut()
+{
+	strChossenUser = "";
+	if(document.getElementById("st-login-form").style.display == "block")
+	{
+		closeForm();
+	} else 
+	{
+	document.getElementById("st-login-form").style.display = "block";
+	}
+}
 function info()
 {
 	window.alert(strChossenUser);
 }
+function search ()
+{
+	if (document.getElementById("search-button").value == "")
+		{
+		 document.getElementById("st-content").innerHTML = arrCategories[arrCategories.length-1];
+		} else
+		{
+		let strSearchResult = '';
+		let strName = "";
+		console.log(document.getElementById("search-button").value);
+		for (var j = 0; j < dataProducts.value.length; j++)
+		{
+			strName = dataProducts.value[j].ProductName;
+			if(strName.includes(document.getElementById("search-button").value))
+			{
+				strSearchResult += '<div class = "st-product">' + dataProducts.value[j].ProductName + '<br>' + Math.floor(dataProducts.value[j].UnitPrice) + '€ <div class = "st-details">' +
+				dataProducts.value[j].QuantityPerUnit + '<input type="number" id="st-quantity'+ dataProducts.value[j].ProductID +'"><button onclick ="buy('+ dataProducts.value[j].ProductID +')">Buy</button></div></div>';
+			}
+			document.getElementById("st-content").innerHTML = strSearchResult;
+		}
+	}
+}
+
 function checkInfo ()
 {
 	let strUsername = document.getElementById("username").value;
@@ -57,6 +127,7 @@ function checkInfo ()
 				{
 					boolLogedIn = true;
 					document.getElementById("st-name").innerHTML = ('Welcome ' + data.value[i].FirstName);
+					intUserID = i;
 					strChossenUser = "";
 					strChossenUser += 'From ' +  data.value[i].Address;
 					strChossenUser +=  ' , ' + data.value[i].Country + ' , ';
@@ -79,25 +150,27 @@ function start() {
 		.then(function (data) {
 			fetch('https://services.odata.org/V3/Northwind/Northwind.svc/Products?$format=json')
 				.then((resp2) => resp2.json())
-				.then(function (data2) {
-					strContent += '<select onchange = "onChange(this)" id ="st-categories" name = "categories"><option value="'+ (data.value.length+1) + '">All categories</option>';
+				.then(function (products) {
+					dataProducts = products;
+					strCategories = "";
+					strCategories += '<select onchange = "onChange(this)" id ="st-categories" name = "categories"><option value="'+ (data.value.length+1) + '">All categories</option>';
 					for (var i = 0; i < data.value.length; i++) {
-						strContent += '<option value = "' + data.value[i].CategoryID + '">' + data.value[i].CategoryName + '</option>';
+						strCategories += '<option value = "' + data.value[i].CategoryID + '">' + data.value[i].CategoryName + '</option>';
 					}	
 					
-					strContent += '</select>'
-					document.getElementById("st-select").innerHTML = (strContent);
+					strCategories += '</select>'
+					document.getElementById("st-select").innerHTML = (strCategories);
 					strContent = "";
 					for (var i = 1; i < data.value.length + 1; i++) {
 						arrCategories[i] = "";
-						for (var j = 0; j < data2.value.length; j++) {
-							if (i == data2.value[j].CategoryID) {
-								arrCategories[i] += '<div class = "st-product">' + data2.value[j].ProductName + '<br>' + Math.floor(data2.value[j].UnitPrice) + '€ <div class = "st-details">' +
-									data2.value[j].QuantityPerUnit + '<input type="number" id="st-quantity'+ data2.value[j].ProductID +'"><button onclick ="buy('+ data2.value[j].ProductID +')">Buy</button></div></div>';
+						for (var j = 0; j < products.value.length; j++) {
+							if (i == products.value[j].CategoryID) {
+								arrCategories[i] += '<div class = "st-product">' + products.value[j].ProductName + '<br>' + Math.floor(products.value[j].UnitPrice) + '€ <div class = "st-details">' +
+									products.value[j].QuantityPerUnit + '<input type="number" id="st-quantity'+ products.value[j].ProductID +'"><button onclick ="buy('+ products.value[j].ProductID +')">Buy</button></div></div>';
 							}
 							if (boolDoOnce) {
-								strContent += '<div class = "st-product" id = " ' + data2.value[j].ProductID + '">' + data2.value[j].ProductName + '<br>' + Math.floor(data2.value[j].UnitPrice) + '€ <div class = "st-details">Per unit: ' +
-									data2.value[j].QuantityPerUnit + '<input type="number" id="st-quantity'+ data2.value[j].ProductID +'"><button onclick ="buy('+ data2.value[j].ProductID +')">Buy</button></div> </div>';
+								strContent += '<div class = "st-product" id = " ' + products.value[j].ProductID + '">' + products.value[j].ProductName + '<br>' + Math.floor(products.value[j].UnitPrice) + '€ <div class = "st-details">Per unit: ' +
+									products.value[j].QuantityPerUnit + '<input type="number" id="st-quantity'+ products.value[j].ProductID +'"><button onclick ="buy('+ products.value[j].ProductID +')">Buy</button></div> </div>';
 							}
 						}
 						arrCategories[(data.value.length+1)] = strContent;
